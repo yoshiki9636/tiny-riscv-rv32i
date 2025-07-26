15e269c630f4ed447cbb30ece07d30bc28d44697
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2025-07-26 12:16:37 +0900

    fixed signal changes on fpga_top

--- sim/simtop.v
+++ sim/simtop.v
@@ -20,30 +20,28 @@ wire [3:0] sio;
 reg interrupt_0;
 reg rx;
 wire tx;
-wire [3:0] rgb_led; 
+wire [2:0] rgb_led; 
+
+wire [1:0] init_latency = 2'd0; 
+wire init_qspicmd = 1'b0;
+wire init_cpu_start = 1'b0;
+wire [1:0] init_uart = 2'd0; 
 
 fpga_top fpga_top (
-	.clk(clk),
+	.clkin(clk),
 	.rst_n(rst_n),
 	.interrupt_0(interrupt_0),
-	.sck(sck),
-	.ce_n(ce_n),
-	.sio(sio),
 	.rx(rx),
 	.tx(tx),
-	.rgb_led(rgb_led)
+	.rgb_led(rgb_led),
+	.gpio(gpio),
+	.init_latency(init_latency),
+	.init_qspicmd(init_qspicmd),
+	.init_cpu_start(init_cpu_start),
+	.init_uart(init_uart)
 	);
 
-// psram model
-qspi_psram_model qspi_psram_model (
-	.clk(clk),
-	.rst_n(rst_n),
-	.sck(sck),
-	.ce_n(ce_n),
-	.sio(sio)
-	);
-
-initial $readmemh("./test.txt", qspi_psram_model.qspi_1r1w.ram);
+initial $readmemh("./test.txt", fpga_top.qspi_innermem.qspi_psram_model.qspi_1r1w.ram);
 
 initial clk = 0;
 
@@ -58,13 +56,25 @@ initial begin
 	rst_n = 1'b0;
 #20
 	rst_n = 1'b1;
-#100
+#1000
 	force fpga_top.cpu_start = 1'b1;
     //force fpga_top.cpu_start_adr = 30'h00000400; // 0x1000 for C
     //force fpga_top.cpu_start_adr = 30'h00000040; // 0x100 for asm
     force fpga_top.cpu_start_adr = 30'h00000000; // 0x0 for test
 #10
 	force fpga_top.cpu_start = 1'b0;
+#1000000
+	interrupt_0 = 1'b1;
+#10000
+	interrupt_0 = 1'b0;
+#1000000
+	interrupt_0 = 1'b1;
+#10000
+	interrupt_0 = 1'b0;
+#1000000
+	interrupt_0 = 1'b1;
+#10000
+	interrupt_0 = 1'b0;
 #1000000
 	$stop;
 end
